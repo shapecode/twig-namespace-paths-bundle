7af4d979b2f51fea93d12c2fa9c766084a747795
Nikita Loges <nl@tenolo.de>
2018-01-24 13:27:36 +0100

    remove stuff

--- src/Liip/FilesystemLoader.php
+++ src/Liip/FilesystemLoader.php
@@ -2,9 +2,7 @@
 
 namespace Shapecode\Bundle\TwigNamespacePathsBundle\Liip;
 
-use Liip\ThemeBundle\ActiveTheme;
 use Liip\ThemeBundle\Twig\Loader\FilesystemLoader as BaseFilesystemLoader;
-use Symfony\Component\Templating\TemplateReferenceInterface;
 
 /**
  * Class FilesystemLoader
@@ -17,33 +15,7 @@ class FilesystemLoader extends BaseFilesystemLoader
 {
 
     /**
-     * @var ActiveTheme|null
-     */
-    protected $activeTheme;
-
-    /**
-     * Define the active theme
-     *
-     * @param ActiveTheme $activeTheme
-     */
-    public function setActiveTheme(ActiveTheme $activeTheme = null)
-    {
-        $this->activeTheme = $activeTheme;
-    }
-
-    /**
-     * Returns the path to the template file.
-     *
-     * The file locator is used to locate the template when the naming convention
-     * is the symfony one (i.e. the name can be parsed).
-     * Otherwise the template is located using the locator from the twig library.
-     *
-     * @param string|TemplateReferenceInterface $template The template
-     * @param bool                              $throw    When true, a \Twig_Error_Loader exception will be thrown if a template could not be found
-     *
-     * @return string The path to the template file
-     *
-     * @throws \Twig_Error_Loader if the template could not be found
+     * @inheritdoc
      */
     protected function findTemplate($template, $throw = true)
     {
