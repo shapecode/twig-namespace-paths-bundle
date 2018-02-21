18d26ccfaa92ea7f24ed51b86d743c5f193e169d
Nikita Loges <nl@tenolo.de>
2018-02-21 15:16:42 +0100

    back to the roots

--- src/DependencyInjection/Compiler/TwigNamespaceCompiler.php
+++ src/DependencyInjection/Compiler/TwigNamespaceCompiler.php
@@ -2,6 +2,7 @@
 
 namespace Shapecode\Bundle\TwigNamespacePathsBundle\DependencyInjection\Compiler;
 
+use Symfony\Component\Config\Resource\FileExistenceResource;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
@@ -13,7 +14,7 @@ use Symfony\Component\HttpKernel\Kernel;
  * @author  Nikita Loges
  * @company tenolo GbR
  */
-class TwigNamespaceCompiler extends AbstractCompiler implements CompilerPassInterface
+class TwigNamespaceCompiler implements CompilerPassInterface
 {
 
     /**
@@ -66,5 +67,48 @@ class TwigNamespaceCompiler extends AbstractCompiler implements CompilerPassInte
         }
 
         return $name;
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     *
+     * @return \Symfony\Component\DependencyInjection\Definition
+     */
+    protected function getTwigFileSystemLoader(ContainerBuilder $container)
+    {
+        return $container->getDefinition('twig.loader.filesystem');
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     *
+     * @return array
+     */
+    protected function getBundleHierarchy(ContainerBuilder $container)
+    {
+        $bundleHierarchy = [];
+
+        foreach ($container->getParameter('kernel.bundles_metadata') as $name => $bundle) {
+            if (!array_key_exists($name, $bundleHierarchy)) {
+                $bundleHierarchy[$name] = [
+                    'metadata' => $bundle,
+                    'paths'    => [],
+                ];
+            }
+
+            if (is_dir($dir = $container->getParameter('kernel.root_dir') . '/Resources/' . $name . '/views')) {
+                $bundleHierarchy[$name]['paths'][] = $dir;
+            }
+            $container->addResource(new FileExistenceResource($dir));
+
+            if (is_dir($dir = $bundle['path'] . '/Resources/views')) {
+                $bundleHierarchy[$name]['paths'][] = $dir;
+            }
+            $container->addResource(new FileExistenceResource($dir));
+
+            $bundleHierarchy[$name]['template_dir'] = $bundle['path'] . '/Resources/templates';
+        }
+
+        return $bundleHierarchy;
     }
 }
